2c487324a28e7adbcc3a1f056deffa83a0bc4e06
janaalhazmi <janaalhazmi@gmail.com>
2026-02-24 02:56:44 +0000

    tasks 0 - 11

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _islower - checks lowercase
+ * @c: character
+ * Return: 1 if lowercase, 0 otherwise
+ */
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+		return (1);
+
+	return (0);
+}
---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _abs - computes absolute value
+ * @n: number
+ * Return: absolute value
+ */
+int _abs(int n)
+{
+	if (n < 0)
+		return (-n);
+
+	return (n);
+}
