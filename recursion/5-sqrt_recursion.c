6288abb7968bfabcce7bb9bba8ce19664886f2ff
janaalhazmi <janaalhazmi78@gmail.com>
2026-03-24 06:03:26 +0000

    all

---
+++ recursion/5-sqrt_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+int helper(int n, int i)
+{
+    if (i * i == n)
+        return i;
+
+    if (i * i > n)
+        return -1;
+
+    return helper(n, i + 1);
+}
+
+int _sqrt_recursion(int n)
+{
+    if (n < 0)
+        return -1;
+
+    return helper(n, 0);
+}
