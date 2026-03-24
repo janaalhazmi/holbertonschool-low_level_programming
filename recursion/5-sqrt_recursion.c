0176aa7e761c758691ef451b7096b6c82ef28d2b
janaalhazmi <janaalhazmi78@gmail.com>
2026-03-24 06:16:19 +0000

    all

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -2,19 +2,19 @@
 
 int helper(int n, int i)
 {
-    if (i * i == n)
-        return i;
+	if (i * i == n)
+		return (i);
 
-    if (i * i > n)
-        return -1;
+	if (i * i > n)
+		return (-1);
 
-    return helper(n, i + 1);
+	return (helper(n, i + 1));
 }
 
 int _sqrt_recursion(int n)
 {
-    if (n < 0)
-        return -1;
+	if (n < 0)
+		return (-1);
 
-    return helper(n, 0);
+	return (helper(n, 0));
 }
--- recursion/6-is_prime_number.c
+++ recursion/6-is_prime_number.c
@@ -2,19 +2,19 @@
 
 int check_prime(int n, int i)
 {
-    if (i == n)
-	return 1;
+	if (i == n)
+		return (1);
 
-    if (n % i == 0)
-	return 0;
+	if (n % i == 0)
+		return (0);
 
-    return check_prime(n, i + 1);
+	return (check_prime(n, i + 1));
 }
 
 int is_prime_number(int n)
 {
-    if (n <= 1)
-	return 0;
+	if (n <= 1)
+		return (0);
 
-    return check_prime(n, 2);
+	return (check_prime(n, 2));
 }
