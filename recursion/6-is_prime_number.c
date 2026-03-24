91968eaa4e79fbd6fcfd212abf33fce9eff3d6fa
janaalhazmi <janaalhazmi78@gmail.com>
2026-03-24 06:06:04 +0000

    all

--- recursion/6-is_prime_number.c
+++ recursion/6-is_prime_number.c
@@ -3,10 +3,10 @@
 int check_prime(int n, int i)
 {
     if (i == n)
-        return 1;
+	return 1;
 
     if (n % i == 0)
-        return 0;
+	return 0;
 
     return check_prime(n, i + 1);
 }
@@ -14,7 +14,7 @@ int check_prime(int n, int i)
 int is_prime_number(int n)
 {
     if (n <= 1)
-        return 0;
+	return 0;
 
     return check_prime(n, 2);
 }
