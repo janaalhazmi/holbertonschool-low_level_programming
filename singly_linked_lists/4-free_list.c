348b0bac20284df33fcf2162bf79e326833f8227
Jana Alhazmi <janaalhazmi78@gmail.com>
2026-04-24 16:23:32 +0300

    Refactor free_list function for clarity

--- singly_linked_lists/4-free_list.c
+++ singly_linked_lists/4-free_list.c
@@ -7,15 +7,15 @@
  */
 void free_list(list_t *head)
 {
-    list_t *temp;
+	list_t *temp;
 
-    while (head != NULL)
-    {
-        temp = head->next;
+	while (head != NULL)
+	{
+		temp = head->next;
 
-        free(head->str);
-        free(head);
+		free(head->str);
+		free(head);
 
-        head = temp;
-    }
+		head = temp;
+	}
 }
